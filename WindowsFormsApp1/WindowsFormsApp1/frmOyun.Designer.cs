a40d92286ae37b0c1eb109b5e0729880bd79a20e
hasancankivrak <58665751+hasancankivrak@users.noreply.github.com>
2020-11-14 22:31:16 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/frmOyun.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmOyun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmr_game = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlPuan = new System.Windows.Forms.Panel();
+            this.tmr_sure = new System.Windows.Forms.Timer(this.components);
+            this.pnlPuan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmr_game
+            // 
+            this.tmr_game.Interval = 1000;
+            this.tmr_game.Tick += new System.EventHandler(this.tmr_game_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pnlPuan
+            // 
+            this.pnlPuan.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlPuan.Controls.Add(this.label1);
+            this.pnlPuan.Controls.Add(this.label2);
+            this.pnlPuan.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlPuan.Location = new System.Drawing.Point(963, 0);
+            this.pnlPuan.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlPuan.Name = "pnlPuan";
+            this.pnlPuan.Size = new System.Drawing.Size(104, 554);
+            this.pnlPuan.TabIndex = 2;
+            // 
+            // tmr_sure
+            // 
+            this.tmr_sure.Interval = 1000;
+            this.tmr_sure.Tick += new System.EventHandler(this.tmr_sure_Tick);
+            // 
+            // frmOyun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.pnlPuan);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmOyun";
+            this.Text = "frmOyun";
+            this.Load += new System.EventHandler(this.frmOyun_Load);
+            this.pnlPuan.ResumeLayout(false);
+            this.pnlPuan.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmr_game;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlPuan;
+        private System.Windows.Forms.Timer tmr_sure;
+    }
+}
